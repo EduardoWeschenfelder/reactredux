8d0300e2a9b64765768fee0202f3eba530c017bc
Eduardo Barbosa Weschenfelder <43359067+EduardoWeschenfelder@users.noreply.github.com>
2020-05-15 17:46:45 -0300

    ReactREDUX

---
+++ src/store/index.js
@@ -0,0 +1,39 @@
+import { createStore } from "redux";
+
+const INITIAL_STATE = {
+  activeLesson: {},
+  activeModule: {},
+  modules: [
+    {
+      id: 1,
+      title: "iniciando com react",
+      lessons: [
+        { id: 1, title: "primeira aula" },
+        { id: 2, title: "segunda aula" },
+      ],
+    },
+    {
+      id: 2,
+      title: "iniciando com Redux",
+      lessons: [
+        { id: 3, title: "terceira aula" },
+        { id: 4, title: "quarta aula" },
+      ],
+    },
+  ],
+};
+
+function reducer(state = INITIAL_STATE, action) {
+  if (action.type === "TOGGLE_LESSON") {
+    return {
+      ...state,
+      activeLesson: action.lesson,
+      activeModule: action.module,
+    };
+  }
+  return state;
+}
+
+const store = createStore(reducer);
+
+export default store;
