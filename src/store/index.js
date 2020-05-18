2bade3f267e19f906efb24c8b583fa5c7d506848
Eduardo Barbosa Weschenfelder <43359067+EduardoWeschenfelder@users.noreply.github.com>
2020-05-18 09:32:56 -0300

    finalizado
    redux compartinhando fuxo de informações

--- src/store/index.js
+++ src/store/index.js
@@ -1,39 +1,7 @@
 import { createStore } from "redux";
 
-const INITIAL_STATE = {
-  activeLesson: {},
-  activeModule: {},
-  modules: [
-    {
-      id: 1,
-      title: "iniciando com react",
-      lessons: [
-        { id: 1, title: "primeira aula" },
-        { id: 2, title: "segunda aula" },
-      ],
-    },
-    {
-      id: 2,
-      title: "iniciando com Redux",
-      lessons: [
-        { id: 3, title: "terceira aula" },
-        { id: 4, title: "quarta aula" },
-      ],
-    },
-  ],
-};
+import rootReducer from "./reducers";
 
-function reducer(state = INITIAL_STATE, action) {
-  if (action.type === "TOGGLE_LESSON") {
-    return {
-      ...state,
-      activeLesson: action.lesson,
-      activeModule: action.module,
-    };
-  }
-  return state;
-}
-
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 export default store;
---
+++ src/store/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+import course from "./couse";
+// import user from './couse';
+
+export default combineReducers({
+  course,
+  //user
+});
